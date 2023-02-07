4c7081775d65e2950b89b10c1662352029344d49
ngocthang2021 <ngnngthang@gmail.com>
2023-02-08 02:30:48 +0700

    TMDB Api w/ axios

---
+++ src/services/api/TMDBApi.js
@@ -0,0 +1,41 @@
+import axiosClient from './axiosClient';
+
+const TMDBApi = {
+	getTrendings: (mediaType, time, params) => {
+		const url = `/trending/${mediaType}/${time}`;
+		return axiosClient.get(url, { params });
+	},
+
+	getDetails: (mediaType, id, params) => {
+		const url = `/${mediaType}/${id}`;
+		return axiosClient.get(url, { params });
+	},
+
+	getLists: (mediaType, queryString, params) => {
+		const url = `/${mediaType}/${queryString}`;
+		return axiosClient.get(url, { params });
+	},
+
+	getSearch: (query, searchQuery) => {
+		const url = `/search/${query}/${searchQuery}`;
+		return axiosClient.get(url);
+	},
+
+	getCollections: (collectionId, params) => {
+		const url = `/collection/${collectionId}`;
+		return axiosClient.get(url, { params });
+	},
+
+	getGenres: (mediaType) => {
+		const url = `/genre/${mediaType}/list`;
+		return axiosClient.get(url);
+	},
+
+	getPerson: (personId, query = '') => {
+		query || query !== ''
+			? axiosClient.get(`person/${personId}/${query}`)
+			: axiosClient.get(`/person/${personId}`);
+	},
+};
+
+export default TMDBApi;
--- src/services/api/axiosClient.js
+++ src/services/api/axiosClient.js
@@ -1,75 +1,27 @@
 import axios from 'axios';
 import queryString from 'query-string';
 
-import {
-	AWS_API_KEY,
-	IGDB_BASE_URL,
-	TWITCH_AUTH_URL,
-	TWITCH_CLIENT_ID,
-	TWITCH_CLIENT_SECRET,
-} from '@constants/IGDB';
-
-const getAccessToken = async () => {
-	// Get Token from localStorage
-	let token = JSON.parse(window.localStorage.getItem('accessToken'));
-
-	// Check if token exists or token expired or not
-	if (!token || new Date(token.expiry) <= new Date()) {
-		// request for Access Token
-		const response = await axios({
-			method: 'post',
-			url: TWITCH_AUTH_URL,
-			params: {
-				client_id: TWITCH_CLIENT_ID,
-				client_secret: TWITCH_CLIENT_SECRET,
-				grant_type: 'client_credentials',
-			},
-		});
-
-		token = {
-			...response.data,
-			// Add Token Expiration Date
-			expiry: new Date().getTime() + response.data.expires_in * 1000,
-		};
-
-		// Save Access Token to localStorage
-		window.localStorage.setItem('accessToken', JSON.stringify(token));
-
-		return token;
-	}
-
-	return token;
-};
+import { TMDB_API_KEY, TMDB_BASE_URL } from '@constants/tmdb';
 
 const axiosClient = axios.create({
-	baseURL: `${IGDB_BASE_URL}/v4`,
-	method: 'post',
+	baseURL: TMDB_BASE_URL,
 	headers: {
-		Accept: 'application/json',
-		'Client-ID': TWITCH_CLIENT_ID,
-		'x-api-key': AWS_API_KEY,
+		'Content-Type': 'application/json',
 	},
-	// paramsSerializer: { serialize: (params) => queryString.stringify({ ...params }) },
+	paramsSerializer: {
+		serialize: (params) => queryString.stringify({ ...params, api_key: TMDB_API_KEY }),
+	},
 });
 
-axiosClient.interceptors.request.use(async (config) => {
-	const token = await getAccessToken();
-
-	// Add Token Authorization to headers
-	if (token) {
-		config.headers.Authorization = `Bearer ${token.access_token}`;
-	}
-
-	return config;
-});
+axiosClient.interceptors.request.use(async (config) => config);
 
 axiosClient.interceptors.response.use(
 	(response) => {
+		if (response && response.data) return response.data;
 		return response;
 	},
 	(error) => {
 		console.log(error);
-		console.log(`${error.message} - ${error.code}`);
 	},
 );
 
