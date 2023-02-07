4efbf681ec0dca11277f6a6c5a3b3b104ca11ad0
ngocthang2021 <ngnngthang@gmail.com>
2023-02-08 02:30:23 +0700

    Page Routing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,32 +1,28 @@
 import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 
-import axiosClient from '@services/api/axiosClient';
+import Hero from '@components/hero/Hero';
+import Slider from '@components/slider/Slider';
+import categories from '@constants/categories';
+
+const Wrapper = styled.section`
+	width: 100%;
+`;
 
 function Home() {
-	const [data, setData] = useState([]);
-
-	useEffect(() => {
-		const getData = async () => {
-			try {
-				const response = await axiosClient({
-					url: '/games',
-					data: 'fields age_ratings,aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,checksum,collection,cover,created_at,dlcs,expanded_games,expansions,external_games,first_release_date,follows,forks,franchise,franchises,game_engines,game_localizations,game_modes,genres,hypes,involved_companies,keywords,language_supports,multiplayer_modes,name,parent_game,platforms,player_perspectives,ports,rating,rating_count,release_dates,remakes,remasters,screenshots,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos,websites',
-				});
-
-				if (!response) throw new Error('Unable to Fetch Data!');
-
-				console.log(response);
-
-				return response;
-			} catch (error) {
-				console.log(error);
-			}
-		};
-
-		getData();
-	}, []);
-
-	return <div>Home</div>;
+	return (
+		<Wrapper>
+			<Hero />
+			<>
+				{categories.map((item, index) => (
+					<Slider
+						key={index}
+						item={item}
+					/>
+				))}
+			</>
+		</Wrapper>
+	);
 }
 
 export default Home;
--- src/routes/Routing.jsx
+++ src/routes/Routing.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import Home from '@pages/Home';
+import Genres from '@pages/Genres';
+import Category from '@pages/Category';
+import Movie from '@pages/Movie';
+import Media from '@pages/Media';
+import Search from '@pages/Search';
 
 function Routing() {
 	return (
@@ -9,6 +14,46 @@ function Routing() {
 			<Route
 				path='/'
 				element={<Home />}
+			/>
+			<Route
+				path='/genres'
+				element={<Genres />}
+			/>
+			<Route
+				path='/genres/:genreID'
+				element={<Genres />}
+			/>
+			<Route
+				path='/category'
+				element={<Category />}
+			/>
+			<Route
+				path='/category/:mediaType'
+				element={<Category />}
+			/>
+			<Route
+				path='/movie'
+				element={<Movie />}
+			/>
+			<Route
+				path='/movie/:movieID'
+				element={<Movie />}
+			/>
+			<Route
+				path='/media'
+				element={<Media />}
+			/>
+			<Route
+				path='/media/:mediaType'
+				element={<Media />}
+			/>
+			<Route
+				path='/search'
+				element={<Search />}
+			/>
+			<Route
+				path='/search/:searchQuery'
+				element={<Search />}
 			/>
 		</Routes>
 	);
