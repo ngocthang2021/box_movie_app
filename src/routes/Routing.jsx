808bf3372d34392fff76fd9ffc7539712ece49ec
ngocthang2021 <ngnngthang@gmail.com>
2023-02-13 20:39:59 +0700

    Update Routing w/ Movie Detail Page

--- src/routes/Routing.jsx
+++ src/routes/Routing.jsx
@@ -4,8 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from '@pages/Home';
 import Genres from '@pages/Genres';
 import Category from '@pages/Category';
-import Movie from '@pages/Movie';
-import Media from '@pages/Media';
+import Detail from '@pages/Detail';
 import Search from '@pages/Search';
 
 function Routing() {
@@ -31,21 +30,10 @@ function Routing() {
 				path='/category/:mediaType'
 				element={<Category />}
 			/>
+
 			<Route
-				path='/movie'
-				element={<Movie />}
-			/>
-			<Route
-				path='/movie/:movieID'
-				element={<Movie />}
-			/>
-			<Route
-				path='/media'
-				element={<Media />}
-			/>
-			<Route
-				path='/media/:mediaType'
-				element={<Media />}
+				path='/:mediaType/:id'
+				element={<Detail />}
 			/>
 			<Route
 				path='/search'
