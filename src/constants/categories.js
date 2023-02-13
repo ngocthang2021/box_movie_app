a702fd0cf517b7c685a4d69b2314dcfcb96c79fb
ngocthang2021 <ngnngthang@gmail.com>
2023-02-13 20:35:58 +0700

    Categories Constants

--- src/constants/categories.js
+++ src/constants/categories.js
@@ -3,8 +3,8 @@ const categories = [
 	{ mediaType: 'movie', query: 'top_rated', title: 'Top Rated Movies' },
 	{ mediaType: 'movie', query: 'popular', title: 'Popular Movies' },
 	{ mediaType: 'movie', query: 'now_playing', title: 'Now Playing' },
-	{ mediaType: 'tv', query: 'top_rated', title: 'Top Rated TV Series' },
-	{ mediaType: 'tv', query: 'popular', title: 'Popular Series' },
+	// { mediaType: 'tv', query: 'top_rated', title: 'Top Rated TV Series' },
+	// { mediaType: 'tv', query: 'popular', title: 'Popular Series' },
 ];
 
 export default categories;
