26ea543c5c1fbf0c5eabacd934fd89e6bada9ed9
ngocthang2021 <ngnngthang@gmail.com>
2023-02-14 17:09:20 +0700

    Button Component

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 const Btn = styled.button`
 	display: flex;
@@ -9,6 +9,7 @@ const Btn = styled.button`
 	max-width: 120px;
 	max-height: 50px;
 	padding: 12px 20px;
+	border-radius: inherit;
 
 	font-family: ${({ theme }) => theme.primaryFont};
 	font-size: 16px;
@@ -18,10 +19,45 @@ const Btn = styled.button`
 
 	transition: all 0.25s ease;
 	cursor: pointer;
+
+	${({ btnType, theme }) => {
+		switch (btnType) {
+			case 'primary':
+				return css`
+					color: ${theme.colors.textColor};
+					background-color: ${theme.colors.primaryColor};
+
+					&:hover {
+						color: ${theme.colors.primaryColor};
+						background-color: transparent;
+					}
+				`;
+
+			case 'secondary':
+				return css`
+					color: ${({ theme }) => theme.colors.textColor};
+					background-color: transparent;
+
+					cursor: pointer;
+					transition: all 0.25s ease;
+
+					&:hover {
+						background-color: ${({ theme }) => theme.colors.primaryColor};
+					}
+				`;
+		}
+	}}
 `;
 
-function Button({ children, onClick }) {
-	return <Btn onClick={onClick ? () => onClick() : null}>{children}</Btn>;
+function Button({ children, onClick, btnType }) {
+	return (
+		<Btn
+			btnType={btnType}
+			onClick={onClick ? () => onClick() : null}
+		>
+			{children}
+		</Btn>
+	);
 }
 
 export default Button;
