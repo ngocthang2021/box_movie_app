2ebeecbdcde4b07e9005574d89ae31c0e2a4ea34
ngocthang2021 <ngnngthang@gmail.com>
2023-02-14 17:07:15 +0700

    Adjust Navbar Routing Constants

--- src/components/navbar/constants/navRoutes.js
+++ src/components/navbar/constants/navRoutes.js
@@ -1,7 +1,5 @@
 const navRoutes = [
 	{ path: '/', title: 'Home' },
-	{ path: '/genres', title: 'Explore' },
-	{ path: '/movie', title: 'Movies' },
 	{ path: '/search', title: 'Find your Movies' },
 ];
 
