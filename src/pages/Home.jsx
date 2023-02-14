b4c58f5ddf95b0f64e97dfd8c343d1373cd64dc9
ngocthang2021 <ngnngthang@gmail.com>
2023-02-14 17:04:58 +0700

    Update Responsive and Movie Card

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,41 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 import Hero from '@components/hero/Hero';
 import Slider from '@components/slider/Slider';
 import categories from '@constants/categories';
+import Loader from '@components/ui/Loader';
 
 const Wrapper = styled.section`
 	width: 100%;
 `;
 
 function Home() {
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		setTimeout(() => {
+			setLoading(false);
+		}, 2000);
+	}, []);
+
 	return (
 		<Wrapper>
-			<Hero />
-			<>
-				{categories.map((item, index) => (
-					<Slider
-						key={index}
-						item={item}
-					/>
-				))}
-			</>
+			{loading ? (
+				<Loader />
+			) : (
+				<>
+					<Hero />
+					<>
+						{categories.map((item, index) => (
+							<Slider
+								key={index}
+								item={item}
+							/>
+						))}
+					</>
+				</>
+			)}
 		</Wrapper>
 	);
 }
